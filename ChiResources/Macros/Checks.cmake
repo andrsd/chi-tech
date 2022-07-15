09fd5d4902ddb290358237141f6b6207bde7ecf7
David Andrs <andrsd@gmail.com>
2022-07-15 08:09:30 -0600

    Check that PETSc was build with parmetis

--- ChiResources/Macros/Checks.cmake
+++ ChiResources/Macros/Checks.cmake
@@ -20,3 +20,10 @@ CHECK_SYMBOL_EXISTS(PETSC_HAVE_HYPRE
 if (NOT PETSC_HAVE_HYPRE MATCHES 1)
     message(FATAL_ERROR "PETSc has not been configured with HYPRE\n")
 endif()
+
+CHECK_SYMBOL_EXISTS(PETSC_HAVE_PARMETIS
+                    "${PETSC_ROOT}/include/petscconf.h"
+                    PETSC_HAVE_PARMETIS)
+if (NOT PETSC_HAVE_PARMETIS MATCHES 1)
+    message(FATAL_ERROR "PETSc has not been configured with parmetis\n")
+endif()
